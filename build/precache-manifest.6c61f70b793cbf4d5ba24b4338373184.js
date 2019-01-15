a653b2ef8c614ebf3cc94fdbbb3e86bb1bac0922
lihqi <455711093@qq.com>
2019-01-15 14:32:33 +0800

    add gh-pages

---
+++ build/precache-manifest.6c61f70b793cbf4d5ba24b4338373184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workbox-test/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "514772209f6c05b912e9",
+    "url": "/workbox-test/build/static/js/runtime~main.51477220.js"
+  },
+  {
+    "revision": "fdf4bc3bdbeb91423dc6",
+    "url": "/workbox-test/build/static/js/main.fdf4bc3b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workbox-test/build/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "fdf4bc3bdbeb91423dc6",
+    "url": "/workbox-test/build/static/css/main.07f06a8a.chunk.css"
+  },
+  {
+    "revision": "0284212d28ea90df00e9e962b3c33966",
+    "url": "/workbox-test/build/index.html"
+  }
+];
